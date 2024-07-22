f1ff44e9b260f9c528e8ee6eee6c990481fee14c
Gusta Oliv <oggustavo203@gmail.com>
2024-07-22 16:53:42 -0300

    Estudo de JS 22/07

---
+++ JavaScript/aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 55.5
+console.log(`A velocidade do seu carro é ${vel}Km/h.`) // um 'Escreva' da vida
+
+// Condição SIMPLES
+if (vel > 60){
+    console.log('Você ultrapassou a velocidade permitida! MULTADO!')
+} 
+
+console.log('Dirija sempre usando cinto de segurança!')
