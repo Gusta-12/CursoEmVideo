bb48bcd605167e9dc6140fb64fa8336db68bb810
Gusta Oliv <oggustavo203@gmail.com>
2024-08-19 17:08:48 -0300

    Estudos JS 19/08

---
+++ JavaScript/aula12/ex012.js
@@ -0,0 +1,10 @@
+let agora = new Date()
+let hora = agora.getHours()
+console.log(`Agora são exatamente ${hora}h.`)
+if (hora >= 5 && hora < 12) {
+    console.log('Bom dia!')
+} else if (hora >= 12 && hora <= 18) {
+    console.log('Boa tarde!')
+} else {
+    console.log('Boa noite!')
+}
