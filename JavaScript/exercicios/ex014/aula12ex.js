fe9ca51a1305a97ee27f7a32bf291ed759638549
Gusta Oliv <oggustavo203@gmail.com>
2024-08-26 16:00:26 -0300

    JS mini estudo 26/08/24

--- JavaScript/exercicios/ex014/aula12ex.js
+++ JavaScript/exercicios/ex014/aula12ex.js
@@ -1,1 +1,20 @@
-alert("Olá")
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        // Bom dia!
+        img.src = 'img/manha.png'
+        document.body.style.background = '#e4ccb0'
+    } else if (hora >= 12 && hora < 19) {
+        // Boa tarde!
+        img.src = 'img/tarde.png'
+        document.body.style.background = '#8ccdfa'
+    } else {
+        // Boa noite!
+        img.src = 'img/noite.png'
+        document.body.style.background = '#006a6e'
+    }
+}
