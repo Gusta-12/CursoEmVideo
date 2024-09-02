70388e03375b30b1c2b23c39bb92170150d2c1cc
Gusta Oliv <oggustavo203@gmail.com>
2024-09-02 18:07:51 -0300

    .

---
+++ JavaScript/aula14/teste.js
@@ -0,0 +1,12 @@
+/*
+var c = 1
+while (c <= 5){
+    console.log(c)
+    c++
+}
+*/
+
+// Estrutura de Repetição com Variável de Controle
+for(var c = 1; c <= 5; c++){ // inicio; condição
+    console.log(c)
+}
