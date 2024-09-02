38e1c6dbf2300521a34d75b0b170c2ac94d2c4ee
Gusta Oliv <oggustavo203@gmail.com>
2024-09-02 12:31:20 -0300

    Mini estudo de JS 02/09/24

---
+++ JavaScript/aula13/testes.js
@@ -0,0 +1,24 @@
+/*
+console.log('teste');
+console.log('teste');
+console.log('teste');
+console.log('teste');
+console.log('teste');
+*/
+
+var cont = 1;
+
+/*
+// Estrutura de Repetição com Teste Lógico no INÍCIO!
+while (cont <= 5) {
+    console.log(`Teste ${cont}`);
+    cont++ // c += 1;
+}
+*/
+
+
+// Estrutura de Repetição com Teste Lógico no FINAL!
+do{
+    console.log(`Teste ${cont}`);
+    cont++;
+}while (cont <=5);
