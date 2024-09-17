dd3070da759a4095b857f7b272c054206252d092
Gusta Oliv <oggustavo203@gmail.com>
2024-09-17 10:12:32 -0300

    Estudos JS 17/09
    Finalizando os estudos de JS no curso em video

--- JavaScript/aula16ex/ex018/script.js
+++ JavaScript/aula16ex/ex018/script.js
@@ -1,7 +1,7 @@
-// let num = document.getElementById('fnum')
-// let lista = document.getElementById('flista')
-// let res = document.getElementById('res')
-// let valores = []
+let num = document.getElementById('fnum')
+let lista = document.getElementById('flista')
+let res = document.getElementById('res')
+let valores = []
 
 function isNumero(n=0) { // Vai verificar se o dado é um número válido!
     if(Number(n) >= 1 && Number(n) <= 100) {
@@ -20,20 +20,39 @@ function inLista(n=0, l) { // Vai verificar se o número está no vetor!
 }
 
 function adicionar() {
-    let num = document.getElementById('fnum')
-    let lista = document.getElementById('flista')
-    let res = document.getElementById('res')
-
-    let valores = []
-
-    if (isNumero(num.value) && !inLista(num.value, valores)) { // 2 funções, verifica se é um número | verifica NÃO se está na lista
+    if (isNumero(num.value) && !inLista(num.value, valores)) { // 2 funções, verifica se é um número | verifica se NÃO está na lista
         valores.push(Number(num.value))
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado.`
         lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         alert('[ERRO] Valor inválido OU já encontrado na lista!')
     }
     num.value = ''
     num.focus() // volta para area de digitação automaticamente
 }
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert('[ERRO] Adicione algum valor!')
+    } else {
+        valores.sort()
+                
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+        }
+        media = soma / valores.length
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${valores.length} elementos cadastrados.</p>`
+        res.innerHTML += `<p>Maior valor: ${valores[valores.length - 1]}</p>`
+        res.innerHTML += `<p>Menor valor: ${valores[0]}</p>`
+        res.innerHTML += `<p>Soma dos valores: ${soma}</p>`
+        res.innerHTML += `<p>Média dos valores: ${media}</p>`
+
+    }
+}
+
+// Solução do ERRO que não deixava o script.js funcionar direito: local do "script:src" precisa ser no <body> ao invés do <head>
